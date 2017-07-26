83d58d8729b42f340498041f219d583c6e953984
shyang107 <shyang107@gmail.com>
2017-07-26 13:53:37 +0800

    Add new ability of convert

--- invoices.go
+++ invoices.go
@@ -5,9 +5,12 @@ import (
 	"fmt"
 	"math"
 	"os"
+	"strconv"
 	"strings"
 	"unicode"
 	"unsafe"
+
+	"github.com/cpmech/gosl/chk"
 )
 
 var (
@@ -234,6 +237,74 @@ func isFileExist(filename string) bool {
 		isExist = false
 	}
 	return isExist
+}
+
+// convert ---------------------------------------------------------
+
+// Atob converts string to bool
+func Atob(val string) (bres bool) {
+	if strings.ToLower(val) == "true" {
+		return true
+	}
+	if strings.ToLower(val) == "false" {
+		return false
+	}
+	res, err := strconv.Atoi(val)
+	if err != nil {
+		chk.Panic("cannot parse string representing integer: %s", val)
+	}
+	if res != 0 {
+		bres = true
+	}
+	return
+}
+
+// Atoi converts string to integer
+func Atoi(val string) (res int) {
+	res, err := strconv.Atoi(val)
+	if err != nil {
+		chk.Panic("cannot parse string representing integer number: %s", val)
+	}
+	return
+}
+
+// Atof converts string to float64
+func Atof(val string) (res float64) {
+	res, err := strconv.ParseFloat(val, 64)
+	if err != nil {
+		chk.Panic("cannot parse string representing float number: %s", val)
+	}
+	return
+}
+
+// Itob converts from integer to bool
+//  Note: only zero returns false
+//        anything else returns true
+func Itob(val int) bool {
+	if val == 0 {
+		return false
+	}
+	return true
+}
+
+// Btoi converts flag to interger
+//  Note: true  => 1
+//        false => 0
+func Btoi(flag bool) int {
+	if flag {
+		return 1
+	}
+	return 0
+}
+
+// Btoa converts flag to string
+//  Note: true  => "true"
+//        false => "false"
+func Btoa(flag bool) string {
+	if flag {
+		return "true"
+	}
+	return "false"
 }
 
 // print ---------------------------------------------------------
