a05555f32295ca1853835c409c430f3919026bb2
shyang107 <shyang107@gmail.com>
2017-07-27 18:01:07 +0800

    create models

---
+++ db.go
@@ -0,0 +1,105 @@
+package invoices
+
+import (
+	"log"
+	"os"
+
+	"github.com/jinzhu/gorm"
+	jsoniter "github.com/json-iterator/go"
+	ut "github.com/shyang107/go-twinvoices/util"
+	// use for sqlite
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+)
+
+// DB is database
+var DB *gorm.DB
+
+// initialdb initialize database
+func initialdb() {
+	ut.Verbose = true
+	if ut.IsFileExist(Cfg.DBfilename) {
+		pstat("  > Removing file %q ...\n", Cfg.DBfilename)
+		err := os.Remove(Cfg.DBfilename)
+		if err != nil {
+			panic(err)
+		}
+	}
+	db, err := gorm.Open("sqlite3", os.ExpandEnv(Cfg.DBfilename))
+	if err != nil {
+		panic("failed to connect database")
+	}
+	defer db.Close()
+	//
+	// Migrate the schema
+	db.AutoMigrate(&Invoice{}, &Detail{}, &FileBunker{})
+	db.Model(&Invoice{}).Related(&Detail{}, "uin")
+	// db.Model(&Invoice{}).AddUniqueIndex("idx_invoices_number", "uin")
+	// db.Model(&Invoice{}).AddForeignKey("uin", "details(id)", "RESTRICT", "RESTRICT")
+}
+
+func connectdb() {
+	//初始化并保持连接
+	var err error
+	DB, err = gorm.Open("sqlite3", Cfg.DBfilename)
+	//    DB.LogMode(true)//打印sql语句
+	if err != nil {
+		log.Fatalf("database connect is err: %s", err.Error())
+	} else {
+		// log.Print("connect database is success")
+		ut.Pfyel("* connect database is success\n")
+	}
+	err = DB.DB().Ping()
+	if err != nil {
+		DB.DB().Close()
+		log.Fatalf("Error on opening database connection: %s", err.Error())
+	}
+	DB.Model(&Invoice{}).Related(&Detail{}, "uin")
+}
+
+// DBDumpInvoices get the list from database
+func DBDumpInvoices() ([]Invoice, error) {
+	// err = DB.Find(pinvs).Error
+	// if err == nil {
+	// 	n := DB.Count(&Invoice{}).
+	// 	for i := 0; i < len(pinvs); i++ {
+	// 		DB.Model(pinvs[i]).Association("details").Find(pinvs[i].Details)
+	// 	}
+	// }
+	invs := []Invoice{}
+	DB.Find(&invs)
+	for i := range invs {
+		// DB.Model(invs[i]).Related(&invs[i].UINumber)
+		DB.Model(&invs[i]).Association("details").Find(&invs[i].Details)
+	}
+	return invs, nil
+}
+
+// DBInsertFrom creats records from []*Invoice into database
+func DBInsertFrom(pvs []*Invoice) {
+	for _, v := range pvs {
+		// io.Pforan("# %v", *v)
+		// DB.FirstOrCreate(v)
+		DB.Where(Invoice{UINumber: v.UINumber}).FirstOrCreate(v)
+	}
+}
+
+// DBDumpData dumps all data from db
+func DBDumpData(dumpFilename string) error {
+	pstat("  > Dumping data from database %q ...\n", Cfg.DBfilename)
+	pvs, err := DBDumpInvoices()
+	if err != nil {
+		return err
+	}
+	// for i, p := range pvs {
+	// 	io.Pfgreen2("Rec. %d : %v\n", i+1, p)
+	// }
+	fn := ut.PathKey(dumpFilename) + ".json"
+	pstat("  >> Marshall data in JSON-type, and then write to %q ...\n", fn)
+	b, err := jsoniter.Marshal(&pvs)
+	if err != nil {
+		return err
+	}
+	ut.WriteBytesToFile(fn, b)
+	printSepline(60)
+	return nil
+}
---
+++ filebunker.go
@@ -0,0 +1,125 @@
+package invoices
+
+import (
+	"crypto/sha256"
+	"fmt"
+	"os"
+	"path/filepath"
+	"reflect"
+	"strings"
+	"time"
+
+	"github.com/jinzhu/gorm"
+	"github.com/shyang107/go-twinvoices/util"
+)
+
+// FileBunker use to backup original file of invoices
+type FileBunker struct {
+	Model    gorm.Model `json:"-" gorm:"embedded"`
+	Name     string     `cht:"檔案名稱" json:"name"`
+	Size     int        `cht:"檔案大小" json:"size"`
+	ModAt    time.Time  `cht:"修改時間" json:"modtime_at" sql:"index"` // modification time
+	Encoding string     `cht:"編碼" json:"encoding"`
+	Checksum string     `cht:"檢查碼" json:"checksum"` //sha256
+	Contents []byte     `cht:"內容" json:"-"`
+}
+
+func (f FileBunker) String() string {
+	Sf := fmt.Sprintf
+	location, _ := time.LoadLocation("Local")
+	val := reflect.ValueOf(f) //.Elem()
+	fld := val.Type()
+	var str string
+	var cols = make([]string, 0)
+	for i := 0; i < val.NumField(); i++ {
+		switch fld.Field(i).Name {
+		case "Model":
+			continue
+		case "ModAt":
+			str = Sf("%v", val.Field(i).Interface().(time.Time).In(location))
+		case "Size":
+			str = util.BytesSizeToString(val.Field(i).Interface().(int))
+		case "Contents":
+			str = "[略...]"
+		default:
+			// str = val.Field(i).Interface().(string)
+			str = Sf("%v", val.Field(i).Interface().(string))
+		}
+		cols = append(cols, Sf("%s:%s", fld.Field(i).Tag.Get("cht"), str))
+	}
+	return strings.Join(cols, csvSep)
+}
+
+// TableName : set Detail's table name to be `details`
+func (FileBunker) TableName() string {
+	// custom table name, this is default
+	return "filebunker"
+}
+
+// GetArgsTable :
+func (f *FileBunker) GetArgsTable(title string, lensp int) string {
+	// Sf := fmt.Sprintf
+	location, _ := time.LoadLocation("Local")
+	if len(title) == 0 {
+		title = "原始發票檔案清單"
+	}
+	// var heads = []string{"項次"}
+	_, _, _, heads := util.GetFieldsInfo(FileBunker{}, "cht", "Model")
+	if lensp < 0 {
+		lensp = 0
+	}
+	// heads = append(heads, tmp...)
+	strSize := util.BytesSizeToString(f.Size)
+	table := util.ArgsTableN(title, lensp, heads,
+		f.Name, strSize, f.ModAt.In(location), f.Encoding, f.Checksum, "[略...]")
+	return table
+}
+
+// GetFileBunkerTable returns the table string of the list of []*Detail
+func GetFileBunkerTable(pfbs []*FileBunker, lensp int) string {
+	Sf := fmt.Sprintf
+	location, _ := time.LoadLocation("Local")
+	title := "原始發票檔案清單"
+	heads := []string{"項次"}
+	_, _, _, tmp := util.GetFieldsInfo(FileBunker{}, "cht", "Model")
+	heads = append(heads, tmp...)
+	if lensp < 0 {
+		lensp = 0
+	}
+	var data []interface{}
+	for i, f := range pfbs {
+		strSize := util.BytesSizeToString(f.Size)
+		data = append(data, i+1,
+			f.Name, strSize, Sf("%v", f.ModAt.In(location)), f.Encoding, f.Checksum, "[略...]")
+	}
+	table := util.ArgsTableN(title, lensp, heads, data...)
+	return table
+}
+
+// UpdateFileBunker updates DB
+func (o *Option) UpdateFileBunker() error {
+	fi, err := os.Stat(o.InpFn)
+	if err != nil {
+		return err
+	}
+	if strings.ToLower(o.IfnSuffix) == ".csv" && strings.ToLower(o.IfnEncoding) == "big5" {
+		b, err := util.ReadFile(o.InpFn)
+		if err != nil {
+			return err
+		}
+		sum := fmt.Sprintf("%x", sha256.Sum256(b))
+		fn := filepath.Base(o.InpFn)
+		fb := FileBunker{
+			Name:     fn,
+			Size:     int(fi.Size()),
+			ModAt:    fi.ModTime(),
+			Encoding: o.IfnEncoding,
+			Checksum: sum,
+			Contents: b,
+		}
+		DB.Where(&fb).FirstOrCreate(&fb)
+		// fbs = append(fbs, &fb)
+	}
+	// plog((&fb).GetArgsTable("", 0))
+	return nil
+}
