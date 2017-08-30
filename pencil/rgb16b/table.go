28ca819f4df974b2312c7a04ba4a9f1f0805c3e6
shyang107 <shyang107@gmail.com>
2017-08-31 00:50:59 +0800

    move to new repository `pencil`

--- pencil/rgb16b/table.go
+++
@@ -1,9 +0,0 @@
-package rgb16b
-
-import "golang.org/x/image/colornames"
-
-// Map contains named colors defined in the SVG 1.1 spec.
-var Map = colornames.Map
-
-// Names contains the color names defined in the SVG 1.1 spec.
-var Names = colornames.Names
